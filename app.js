b3fdf1f7376db5d0d655724b386b1ac336c34b8d
Makinde Foluso Samuel <folusosamuel@gmail.com>
2022-04-30 10:39:37 +0100

    ejs-array of objects template 1

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-const express = require("express");
+const express = require('express');
+const ejs = require('ejs');
+const ejsLint = require('ejs-lint');
 
 //express app
 const app = express();
 
-app.get("/", (req, res) => {
+//register view engine
+app.set('view engine', 'ejs'); //automatically express and ejs will look for views folder for ejs files
+
+app.get('/', (req, res) => {
   //res.send(`<p>Home Page</p>`);
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: 'Yoshi finds eggs',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'Mario finds stars',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+    {
+      title: 'How to defeat bowser',
+      snippet: 'Lorem ipsum dolor sit amet consectetur',
+    },
+  ];
+  //res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', blogs });
 });
 
-app.get("/about", (req, res) => {
+app.get('/about', (req, res) => {
   //res.send(`<p>About Page</p>`);
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-//redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
 //404 error!
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render('404');
 });
 
 //listen for requests
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log('listening on port 3000');
 });
